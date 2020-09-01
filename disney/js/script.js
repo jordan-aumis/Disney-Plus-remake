1ae73ce758ab6dda9f7d4fe265a5749665e13f3a
Jordan Aumis <jordan0aumis@gmail.com>
2020-09-01 11:23:50 +0200

    petite modif

--- disney/js/script.js
+++ disney/js/script.js
@@ -1,11 +1,16 @@
+// Ici je recupere mes element du dom
 let body = document.querySelector("main")
 let nav1 = document.querySelector("#nav1")
 let nav2 = document.querySelector("#nav2")
 let nav3 = document.querySelector("#nav3")
+
 console.log(nav2);
 console.log(nav1);
+
 var x = 0;
+// MON CODE POUR TELEPHONE
 if (window.matchMedia("(max-width : 769px)").matches) {
+    // j'utilise l'eventlistener Scroll pour faire apparaitre ou disparaitre mes navs
     window.addEventListener("scroll", function (){
     if (window.scrollY < 300){
         nav1.style.display = "flex"
@@ -26,6 +31,7 @@ if (window.matchMedia("(max-width : 769px)").matches) {
     }
 })
 } 
+// CODE POUR ORDINATEUR
 else {
    window.addEventListener("scroll", function (){
     if (window.scrollY > 600){
@@ -34,8 +40,7 @@ else {
     }
     else if (window.scrollY < 600){
         nav2.style.display = "none"
-        nav1.style.display = "flex" 
-         
+        nav1.style.display = "flex"  
     }
 })
     
